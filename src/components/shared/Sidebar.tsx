d66ec522dd24c09020b8cb032f1eba6849007213
mersadrezazadeh <mersadrezazadeh@gmail.com>
2024-02-20 20:37:27 +0330

    improve sidebar ux

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -12,7 +12,7 @@ function Sidebar() {
 
   return (
     <aside className="hidden min-h-screen w-72 bg-white p-5 shadow-md shadow-purple-200/50 lg:flex">
-      <div className="flex size-full flex-col gap-6">
+      <div className="flex size-full flex-col gap-4">
         <Link href="/" className="flex justify-center md:py-2">
           <Image
             src="/assets/images/logoipsum.svg"
@@ -24,7 +24,34 @@ function Sidebar() {
         <nav className="h-full flex-col justify-between md:flex md:gap-4">
           <SignedIn>
             <ul className="hidden w-full flex-col gap-2 md:flex">
-              {NavLinks.map((link) => {
+              {NavLinks.slice(0, 6).map((link) => {
+                const isActive = pathname === link.route;
+
+                return (
+                  <li
+                    className={`group flex w-full items-center justify-center whitespace-nowrap rounded-full bg-cover font-semibold transition-all hover:to-brand-100 hover:shadow-inner ${isActive ? "bg-gradient text-white" : "text-gray-700"}`}
+                    key={link.route}
+                  >
+                    <Link
+                      href={link.route}
+                      className="flex size-full items-center gap-4 p-4"
+                    >
+                      <Image
+                        src={link.icon}
+                        alt="icon"
+                        width={24}
+                        height={24}
+                        className={`${isActive ? "brightness-200" : ""}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+
+            <ul className="hidden w-full flex-col gap-2 md:flex">
+              {NavLinks.slice(6).map((link) => {
                 const isActive = pathname === link.route;
 
                 return (
